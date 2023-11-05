84142c8598bf34fa99ce8c14efd1a54d2e04a3ec
Andreas Backström <andreas@simplymozzo.se>
2023-11-05 19:10:23 +0100

    Add mock data to datasets page

--- src/pages/Datasets.jsx
+++ src/pages/Datasets.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import data from '../datasets.json'
+import { Button, Table, TextInput, Badge  } from 'flowbite-react';
 
 function Datasets() {
   return (
-    <div>Datasets</div>
+    <div className="container mx-auto py-10">
+      <Table striped hoverable className="border">
+                <Table.Head>
+                    <Table.HeadCell>
+                        ID
+                    </Table.HeadCell>
+                    <Table.HeadCell>
+                        Name
+                    </Table.HeadCell>
+                </Table.Head>
+                <Table.Body>
+                  {data?.map(item => {
+                    return (
+                      <Table.Row>
+                        <Table.Cell>
+                          {item.id}
+                        </Table.Cell>
+                        <Table.Cell>
+                          {item.name}
+                        </Table.Cell>
+                      </Table.Row>
+                    )
+                  })}
+                </Table.Body>
+            </Table>
+     
+    </div>
   )
 }
 
